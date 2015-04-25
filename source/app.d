a18671f0fda6057b99d35b33406628fba037800c
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2015-04-26 01:50:08 +0300

    Cleanup main file

--- source/app.d
+++ source/app.d
@@ -10,38 +10,19 @@ import std.stdio;
 
 void main(string[] args)
 {
-	//Config config = new Config();
-	//config.setRoot(dirName(args[0]));
-	
-	ushort port;
-	string host;
-
 	string configFile;
 
 	getopt(
 		args,
-		"port", &port,
-		"host", &host,
 		"config", &configFile	
 	);
 
-	if (!port) {
-		port = 80;
-	}
-
-	if (!host) {
-		host = "0.0.0.0";
-	}
 
 	if (!configFile) {
 		configFile = "config.json";
 	}
 
 	auto config = ConfigReader.read(configFile, dirName(args[0]));
-	writeln(config);
-
-	//config.setHost(host);
-	//config.setPort(port);
 
     Server server = new Server(config);
     server.run();
